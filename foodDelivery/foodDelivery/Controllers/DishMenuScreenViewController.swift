425596b4cf9a8f1163c0a436bbc62f8bf9c689fb
semenchukou <aliaxeisemenchukov@gmail.com>
2019-05-22 10:26:32 -0700

    added views and controllers

---
+++ foodDelivery/foodDelivery/Controllers/DishMenuScreenViewController.swift
@@ -0,0 +1,31 @@
+//
+//  DishMenuScreenViewController.swift
+//  foodDelivery
+//
+//  Created by Aliaxei on 22/05/2019.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import UIKit
+
+class DishMenuScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        //dfthe
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
