ab8e1430b53f94ea90eec5ee3ed043211341d399
mac <alex100zx@gmail.com>
2019-05-23 10:21:30 -0700

    Add restauran pin class

--- foodDelivery/foodDelivery/Controllers/MapViewController.swift
+++ foodDelivery/foodDelivery/Controllers/MapViewController.swift
@@ -7,16 +7,44 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        locationManager.delegate = self;
+        locationManager.distanceFilter = kCLLocationAccuracyNearestTenMeters;
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest;
+        
+        mapView.delegate = self
+        mapView.showsUserLocation = true
+        mapView.userTrackingMode = .follow
+        
+        setupData()
     }
     
-
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // 1. status is not determined
+        if CLLocationManager.authorizationStatus() == .NotDetermined {
+            locationManager.requestAlwaysAuthorization()
+        }
+            // 2. authorization were denied
+        else if CLLocationManager.authorizationStatus() == .Denied {
+            showAlert("Location services were previously denied. Please enable location services for this app in Settings.")
+        }
+            // 3. we do have authorization
+        else if CLLocationManager.authorizationStatus() == .AuthorizedAlways {
+            locationManager.startUpdatingLocation()
+        }
+    }
     /*
     // MARK: - Navigation
 
---
+++ foodDelivery/foodDelivery/Models/RestaurantPin.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantPin.swift
+//  foodDelivery
+//
+//  Created by mac on 5/23/19.
+//  Copyright © 2019 mac. All rights reserved.
+//
+
+import Foundation
+
+class RestaurantPin {
+    
+    var restaurantName:String?
+    var latitude:Double?
+    var longitude:Double?
+    
+}
