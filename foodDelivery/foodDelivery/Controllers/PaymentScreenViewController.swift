d7ffa958b5031f9d04e61858ca4ce9e31c9275c5
semenchukou <aliaxeisemenchukov@gmail.com>
2019-05-23 17:11:13 -0700

    started developing paymentScreen

--- foodDelivery/foodDelivery/Controllers/PaymentScreenViewController.swift
+++ foodDelivery/foodDelivery/Controllers/PaymentScreenViewController.swift
@@ -11,23 +11,68 @@ import UIKit
 class PaymentScreenViewController: UIViewController {
 
     
+    @IBOutlet weak var addressLabel: UITextField!
+    @IBOutlet weak var cardField: UITextField!
+    @IBOutlet weak var telephoneField: UITextField!
+    @IBOutlet weak var paymentSegment: UISegmentedControl!
     var restaurantName:String?
+    var resultString:String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func paymentChanged(_ sender: Any) {
+        if paymentSegment.selectedSegmentIndex == 0{
+            cardField.isHidden = false
+        }
+        else{
+            cardField.isHidden = true
+        }
     }
-    */
-
+    
+    func checkFields()->Bool{
+        if addressLabel.text == "" || addressLabel.text == "Address"{
+            let alertController = UIAlertController(title: "Error!", message: "Enter address!", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Ок", style: .default, handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+            return false
+        }
+        else if telephoneField.text == "" || telephoneField.text == "Telephone number"{
+            let alertController = UIAlertController(title: "Error!", message: "Enter telephone!", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Ок", style: .default, handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+            return false
+        }
+        else if paymentSegment.selectedSegmentIndex == 0 && (cardField.text == "" || cardField.text == "Card number"){
+            let alertController = UIAlertController(title: "Error!", message: "Enter card number!", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Ок", style: .default, handler: nil))
+            self.present(alertController, animated: true, completion: nil)
+            return false
+        }
+        return true
+        
+    }
+    
+    @IBAction func confirmPressed(_ sender: Any) {
+        if !checkFields(){
+            return
+        }
+        for i in 0..<AppDataCollections.basketAmounts.count{
+            resultString += AppDataCollections.basketNames[i].name! + " x" + String(AppDataCollections.basketAmounts[i]) + ", "
+        }
+        resultString += restaurantName!
+        resultString += ", "
+        resultString += addressLabel.text!
+        resultString += ", "
+        if paymentSegment.selectedSegmentIndex == 0{
+            resultString += cardField.text!
+        }
+        else{
+            resultString += "cash"
+        }
+        //add to plist
+        //delete data from collections
+    }
+    
 }
